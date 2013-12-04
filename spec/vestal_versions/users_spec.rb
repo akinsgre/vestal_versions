96ea8f5518802ea18262067978e03302774f6b51
Greg Akins <angrygreg@gmail.com>
2013-12-03 22:21:13 -0500

    Removing updated_by because it conflicts with ar_audit_trace

--- spec/vestal_versions/users_spec.rb
+++ spec/vestal_versions/users_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe VestalVersions::Users do
-  let(:updated_by){ User.create(:name => 'Steve Jobs') }
+
   let(:user){ User.create(:name => 'Steve Richert') }
 
   it 'defaults to nil' do
@@ -9,13 +9,5 @@ describe VestalVersions::Users do
     user.versions.last.user.should be_nil
   end
 
-  it 'accepts and returns an ActiveRecord user' do
-    user.update_attributes(:first_name => 'Stephen', :updated_by => updated_by)
-    user.versions.last.user.should == updated_by
-  end
 
-  it 'accepts and returns a string user name' do
-    user.update_attributes(:first_name => 'Stephen', :updated_by => updated_by.name)
-    user.versions.last.user.should == updated_by.name
-  end
 end
